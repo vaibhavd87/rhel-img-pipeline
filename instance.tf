88676ce4c8e14c4d3cf3fb44995de2545e15a26f
vaibhavd87 <32495872+vaibhavd87@users.noreply.github.com>
2021-03-30 18:37:21 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -15,7 +15,7 @@ resource "google_compute_instance" "vm" {
    }
   service_account {
     # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-    email  = google_service_account.default.email
+    email  = "1090549738014-compute@developer.gserviceaccount.com"
     scopes = ["cloud-platform"]
   }
    
